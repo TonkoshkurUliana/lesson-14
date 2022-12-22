98fd299f4849745be94633dd66607d33ff83ce1f
Tonkoshkur.Uliana <mikityukus@gmail.com>
2022-12-22 11:39:18 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.TreeSet;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        TreeSet<Student> st = new TreeSet<>(new AgeComparator());
+        st.add(new Student("Vasul",32));
+        st.add(new Student("Roman",30));
+        st.add(new Student("John",12));
+        st.add(new Student("Roman",10));
+        st.add(new Student("Ura",89));
+
+        System.out.println("Відсортований список");
+        for (Student person : st) {
+            System.out.println(person);
+        }
+
     }
 }
